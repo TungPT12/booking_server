c54af128011335b8f1065c5a9f43fcc36a1e2113
TungPT12 <mylifeundermycontrol@gmail.com>
2024-07-28 15:25:18 +0700

    Update mongoose.js

--- src/configs/mongoose.js
+++ src/configs/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const uri = 'mongodb+srv://TungPT:BoyPham1204@mongodbdemo.yxxsulj.mongodb.net/booking_project?retryWrites=true&w=majority';
-const uri = 'mongodb+srv://TungPT:BoyPham1204@mongodb.y8qiteb.mongodb.net/booking_project?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://TungPT:TungPT12345@mongodb.y8qiteb.mongodb.net/booking_project?retryWrites=true&w=majority'
 
 const connectMongo = (callback) => {
     mongoose.connect(uri).then(() => {
